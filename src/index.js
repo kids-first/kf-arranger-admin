86536f3c1938ccb66924a067e597456c498fb4fc
Minh <hlminh2000@gmail.com>
2019-01-10 17:31:51 -0500

    adds status endpoint

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import cors from "cors";
 import express from "express";
 import egoTokenMiddleware from "ego-token-middleware";
 import { ES_HOST, PORT, EGO_URL } from "./env";
+import { name, version } from "../package.json";
+
+const status = ({ startTime }) => (req, res) => {
+  res.send({ name, version, startTime, upTime: Date.now() - startTime });
+};
 
 const start = async () => {
   const app = express();
   app.use(cors());
+
+  app.use("/status", status({ startTime: Date.now() }));
 
   app.use(
     egoTokenMiddleware({
